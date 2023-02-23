f015300a77e27d8c35f9637c71923bedbe6c9f5b
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2023-02-22 23:33:25 -0300

    Countdown criado

---
+++ Front-end/HTML5/Countdown/scripts/script.js
@@ -0,0 +1,10 @@
+
+function contagem() {
+    let contador = 100
+    while (contador != 0) {
+        contador -= 1
+        document.getElementById('horas').innerHTML = contador;
+    }
+    
+}
+    
