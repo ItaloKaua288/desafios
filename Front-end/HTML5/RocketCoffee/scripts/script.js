5dcbceebf8a011aa2bdbcdc1430580069af33063
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2023-03-04 22:50:43 -0300

    update

--- Front-end/HTML5/RocketCoffee/scripts/script.js
+++ Front-end/HTML5/RocketCoffee/scripts/script.js
@@ -1,13 +1,20 @@
 function burguer() {
     const burguerImg = document.getElementById('img-burguer')
     const menu = document.getElementById('menu')
+    const main = document.getElementById('main')
+    const body = document.getElementById('body')
+
     if (menu.style.opacity == '1') {
         menu.style.opacity = '0'
         menu.style.height = '0vh'
         burguerImg.src = 'assets/menu-buguer-close.svg'
+        main.style.opacity = '1'
+        main.style.background = 'transparent'
     } else {
         menu.style.opacity = '1'
         menu.style.height = 'calc(100vh + 160px)'
         burguerImg.src = 'assets/menu-buguer-open.svg'
+        main.style.opacity = '0'
+        main.style.background = 'black'
     }
 }
