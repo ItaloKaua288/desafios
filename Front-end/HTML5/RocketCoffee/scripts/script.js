7c156829658fb9319569c6c693d2de9171e95c88
Ítalo Kauã N. D <122760006+ItaloKaua288@users.noreply.github.com>
2023-03-04 21:48:53 -0300

    update

--- Front-end/HTML5/RocketCoffee/scripts/script.js
+++ Front-end/HTML5/RocketCoffee/scripts/script.js
@@ -7,7 +7,7 @@ function burguer() {
         burguerImg.src = 'assets/menu-buguer-close.svg'
     } else {
         menu.style.opacity = '1'
-        menu.style.height = 'calc(100vh + 165px)'
+        menu.style.height = 'calc(100vh + 160px)'
         burguerImg.src = 'assets/menu-buguer-open.svg'
     }
 }
